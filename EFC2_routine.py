645f93991c961671fc3f9be5e9ba5f08e03c52b3
Ali Ghavampour <53136447+alighavam@users.noreply.github.com>
2024-09-10 17:30:18 -0400

    subject routine update

--- EFC2_routine.py
+++ EFC2_routine.py
@@ -1,8 +1,20 @@
 import os
 import numpy as np
 import pandas as pd
+from utils.movload import movload
 from config import *
 
+def trial_routine(row):
+    '''
+    This function performs all the necessary preprocessing for a single trial
+    '''
+    C = pd.DataFrame()
+
+    # add row to the dataframe:
+    C = pd.concat([C, row], ignore_index=True)
+
+    return C
+    
 def subject_routine(subject, smoothing_window=30, fs=500):
     """
     This function is used to preprocess the data of a subject
@@ -14,6 +26,7 @@ def subject_routine(subject, smoothing_window=30, fs=500):
     """
     # empty dataframe to store the data:
     df = pd.DataFrame()
+    df_mov = pd.DataFrame(columns=['day', 'sn', 'BN', 'TN', 'trial_correct', 'mov'])
 
     # Load each day's data:
     dirs = [entry.name for entry in os.scandir(DATA_PATH) if entry.is_dir()]
@@ -29,4 +42,27 @@ def subject_routine(subject, smoothing_window=30, fs=500):
             dat_file_name = os.path.join(sub_dir,d, f'efc2_{subject}.dat')
             dat = pd.read_csv(dat_file_name, sep='\t')
 
-            
+            oldblock = -1
+            # loop on trials:
+            for i in range(dat.shape[0]):
+                if dat['BN'][i] != oldblock:
+                    print(f'Processing block {dat["BN"][i]}')
+                    # load the .mov file:
+                    mov = movload(os.path.join(sub_dir,d,f'efc2_{subject}_{dat["BN"][i]:02d}.mov'))
+                    oldblock = dat['BN'][i]
+                
+                # trial routine:
+                C = trial_routine(dat.iloc[[i]])
+
+                # append the trial to the dataframe:
+                df = pd.concat([df, C], ignore_index=True)
+
+                # add the mov trial in the move dataframe:
+                tmp = pd.DataFrame({'day': int(d[-1]), 'sn': subject, 'BN': dat['BN'][i], 'TN': dat['TN'][i], 'trial_correct':dat['trialCorr'][i], 
+                                    'mov': np.array(mov[dat['TN'][i]-1])})
+                df_mov = pd.concat([df_mov, tmp],ignore_index=True)
+
+subject_routine(100)
+                
+
+
