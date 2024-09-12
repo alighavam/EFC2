016097fd04141ae7c66e05a295cf363128482648
Ali Ghavampour <aghavamp@AFI-CBS-H-6.robarts.ca>
2024-09-11 21:50:16 -0400

    server modifications

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 import os
+import platform
 
 # Store the current working directory in a variable
 WORKING_DIRECTORY = os.getcwd()
 
 DATA_PATH = WORKING_DIRECTORY + '/data/'
 ANALYSIS_PATH = WORKING_DIRECTORY + '/analysis/'
+
+# If on server:
+if platform.system() == 'Linux':
+    DATA_PATH = '/cifs/diedrichsen/data/SequenceAndChord/ExtFlexChord/efc2/data'
+    ANALYSIS_PATH = '/cifs/diedrichsen/data/SequenceAndChord/ExtFlexChord/efc2/analysis'
